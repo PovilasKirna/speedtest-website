e0a70ce4ae5c23973fd3ecc003868c2707c24ab5
Povilas Kirna <povilas.kirna@gmail.com>
2022-06-20 16:19:20 +0200

    Updated page title and icon

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import TextBox from "./components/TextBox";
 import StartScreen from "./components/StartScreen";
 import EndScreen from "./components/EndScreen";
 import { useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 
 function App() {
 	const { gameStarted, gameEnded } = useSelector((state) => state.gameControl);
 
 	return (
-		<div className="flex flex-col w-screen h-screen m-0 content-center justify-center items-center">
-			{!gameStarted && !gameEnded && <StartScreen />}
-			{gameStarted && !gameEnded && (
-				<>
-					<Timer />
-					<TextBox />
-					<Input />
-				</>
-			)}
-			{gameEnded && <EndScreen />}
-		</div>
+		<>
+			<Helmet>
+				<title>Speed Test</title>
+			</Helmet>
+			<div className="flex flex-col w-screen h-screen m-0 content-center justify-center items-center">
+				{!gameStarted && !gameEnded && <StartScreen />}
+				{gameStarted && !gameEnded && (
+					<>
+						<Timer />
+						<TextBox />
+						<Input />
+					</>
+				)}
+				{gameEnded && <EndScreen />}
+			</div>
+		</>
 	);
 }
 
